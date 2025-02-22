86cbda0e5ba3b02fe3c085689a92d4420db544e4
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2025-02-22 14:04:25 +0000

    Hello World!

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ int main() {
     if(DEV_Module_Init()!=0){
         return -1;
     }
-    printf("Charlie\n");
     EPD_1IN54B_V2_Init();
-    EPD_1IN54B_V2_Clear();
     DEV_Delay_ms(200);
     Paint_Clear(0xFF);
+    const char* str = "Hello World!";
+    Paint_DrawString_EN(0, 0, str, &Font12, BLACK, WHITE);
 }
