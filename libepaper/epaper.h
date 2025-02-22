3fdfdfaface80e53635bf203547eab4ec4fc3a1f
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2025-02-22 12:51:14 +0000

    Initialise the epaper device

---
+++ libepaper/epaper.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <Fonts/fonts.h>
+#include <e-Paper/EPD_1in54b_V2.h>
+#include <Config/dev_hardware_SPI.h>
+#include <Config/DEV_Config.h>
+#include <Config/Debug.h>
+#include <GUI/GUI_BMPfile.h>
+#include <GUI/GUI_Paint.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -1,5 +1,12 @@
-#include <GUI_Paint.h>
+#include <epaper.h>
 
 int main() {
-    Paint_Clear(0);
+    if(DEV_Module_Init()!=0){
+        return -1;
+    }
+    printf("Charlie\n");
+    EPD_1IN54B_V2_Init();
+    EPD_1IN54B_V2_Clear();
+    DEV_Delay_ms(200);
+    Paint_Clear(0xFF);
 }
